80e5ec8448a827d08696cff4f0f559d158140ed0
Yang Jiachen <241040087@hdu.edu.cn>
2025-04-13 19:23:00 +0800

    删除 一个 仓库

--- Awesome-Ollama-Server/src/constants/index.ts
+++
@@ -1,1 +0,0 @@
-export const PAGE_SIZE_OPTIONS = [10, 20, 50, 100];
--- Awesome-Ollama-Server/src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export interface OllamaService {
-  server: string;
-  models: string[];
-  tps: number;
-  lastUpdate: string;
-}
-
-export type SortField = 'tps' | 'lastUpdate';
-export type SortOrder = 'asc' | 'desc'; 
